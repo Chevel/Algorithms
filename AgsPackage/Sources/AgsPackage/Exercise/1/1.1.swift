7cf072b7b00ae4b44c87e7e7672ad91b31347b88
Matej Kokosinek <matej.dev@blauberg.tech>
2025-08-15 08:57:32 +0200

    feat: e1-6

---
+++ AgsPackage/Sources/AgsPackage/Exercise/1/1.1.swift
@@ -0,0 +1,35 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+// 1.1-1
+//
+// - Hearthstone
+// - Hiking trail, GPS navigation
+
+// 1.1-2
+//
+// Hardware limitations
+// Power consumption?
+
+// 1.1-3
+//
+// Array
+// + access is O(1)
+// - but only if you know the position
+
+// 1.1-4
+//
+// Shortest path does not have to visit all stops
+// Salesperson does
+//
+// They both deal with the shortest route
+
+// 1.1-5
+//
+// best: Such a generic question gets a generic answer: Military, Medical field, anywhere large amounts of money are involved.
+// good enough: systems where the experience is lessened, but not broken. ie. Algorithm processing some data where we can afford to wait a bit longer.
+
+// 1.1-6
+//
+// Traveling salesperson - we know all the leads to visit in advance.
+// Multiplayer games - multiple inputs processed in realtime delivered over time.
